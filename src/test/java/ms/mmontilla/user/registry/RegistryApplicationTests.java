7653aaf15532f90e5597d2f62d96791e9ffced09
marcomarco <montilla.marcoa@gmail.com>
2023-11-24 21:09:47 -0300

    build: project scaffolding in order to start the development
    this scaffold was build using spring initlizr page

---
+++ src/test/java/ms/mmontilla/user/registry/RegistryApplicationTests.java
@@ -0,0 +1,12 @@
+package ms.mmontilla.user.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
